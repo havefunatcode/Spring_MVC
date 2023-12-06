60b60e307194e43f2ddeddcb07664026f8810e91
havefunatcode <dlxogh439@gmail.com>
2023-12-06 22:36:19 +0900

    Adapter Pattern

---
+++ src/main/java/hello/springmvc_1/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package hello.springmvc_1.web.frontcontroller.v5;
+
+import hello.springmvc_1.web.frontcontroller.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+
+}
