ea940024a586229d89a1742f405609d768e29ba9
havefunatcode <dlxogh439@gmail.com>
2023-12-30 23:56:20 +0900

    Spring Controller 사용

---
+++ src/main/java/hello/springmvc_1/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,27 @@
+package hello.springmvc_1.web.springmvc.v1;
+
+import hello.springmvc_1.domain.member.Member;
+import hello.springmvc_1.domain.member.MemberRepository;
+import hello.springmvc_1.web.frontcontroller.ModelView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView modelView = new ModelAndView("members");
+        modelView.addObject("members", members);
+
+        return modelView;
+    }
+
+}
